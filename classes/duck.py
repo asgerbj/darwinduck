27fa23cf8ad8defaf653138f254829f3a00a28b3
jagob <jacobvdueholm@hotmail.com>
2013-08-16 21:56:12 +0200

    Added movement and borders

--- classes/duck.py
+++ classes/duck.py
@@ -1,26 +1,74 @@
 import pygame
 import math
+import os
 
 class duck(pygame.sprite.Sprite):
 	def __init__(self,color,x,y):
 		pygame.sprite.Sprite.__init__(self) 
-		self.image = pygame.Surface([30, 50])
-		self.image.fill(color)
-		self.rect = self.image.get_rect()
+		# self.image = pygame.Surface([30, 50])
+		# self.image.fill(color)
+                self.image = pygame.image.load(os.path.join("images/duck1/", "N.png"))
+                self.rect = self.image.get_rect()
 		self.rect.x = x
 		self.rect.y = y
+                self.xpos = float(x)
+                self.ypos = float(y)
 		self.movementdir = 0
 		self.movementspeed = 0
 
 	def right(self):
-		self.movementspeed += 3
+		self.movementspeed += 2
+                self.movementdir -= 30
+                if self.movementdir <= 0:
+                   self.movementdir = 360
+
+                print ""
+                print "movementspeed: " + str(self.movementspeed)
+                print "movementdir:   " + str(self.movementdir)
 
 	def left(self):
-		self.movementspeed += 3
+		self.movementspeed += 2
+                self.movementdir += 30
+                if self.movementdir >= 360:
+                   self.movementdir = 0
+
+                print ""
+                print "movementspeed: " + str(self.movementspeed)
+                print "movementdir:   " + str(self.movementdir)
 		
 	def update(self):
-		print self.movementspeed
-		print self.movementdir
-		self.rect.y -= self.movementspeed
+		# print self.movementspeed
+		self.ypos -= self.movementspeed * math.cos(self.movementdir * math.pi / 180)
+                self.rect.y = int(self.ypos)
+		self.xpos -= self.movementspeed * math.sin(self.movementdir * math.pi / 180) 
+                self.rect.x = int(self.xpos)
 		if self.movementspeed > 0:
-			self.movementspeed -= 0.25
+                   self.movementspeed =  self.movementspeed * 0.95
+
+                if self.movementdir == 0 or self.movementdir == 360:
+                   self.image = pygame.image.load(os.path.join("images/duck1/", "N.png"))
+                
+                if self.movementdir == 30 or self.movementdir == 60:
+                   self.image = pygame.image.load(os.path.join("images/duck1/", "NW.png"))
+
+                if self.movementdir == 90:
+                   self.image = pygame.image.load(os.path.join("images/duck1/", "W.png"))
+
+                if self.movementdir == 120 or self.movementdir == 150:
+                   self.image = pygame.image.load(os.path.join("images/duck1/", "SW.png"))
+
+                if self.movementdir == 180:
+                   self.image = pygame.image.load(os.path.join("images/duck1/", "S.png"))
+
+                if self.movementdir == 210 or self.movementdir == 240:
+                   self.image = pygame.image.load(os.path.join("images/duck1/", "SE.png"))
+
+                if self.movementdir == 270:
+                   self.image = pygame.image.load(os.path.join("images/duck1/", "E.png"))
+
+                if self.movementdir == 300 or self.movementdir == 330:
+                   self.image = pygame.image.load(os.path.join("images/duck1/", "NE.png"))
+
+
+                self.rect.y += 1
+                self.ypos += 1
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ if not pygame.mixer: print 'Warning, sound disabled'
 
 # Colourcodes
 black = ( 0, 0, 0)
+blue  = ( 0, 255, 0)
 white = ( 255, 255, 255)
 bluewater = ( 25, 155, 225)
 
@@ -15,18 +16,71 @@ bluewater = ( 25, 155, 225)
 sys.path.append("classes")
 from duck import duck
 
+class Platform (pygame.sprite.Sprite):
+    def __init__(self, color, width, height):
+        pygame.sprite.Sprite.__init__(self)
+ 
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+ 
+        self.rect = self.image.get_rect()
+    def update(self):
+        self.rect.y += 1
+
+
 # Initialize pygame
 pygame.init() # Launch pygame
 pygame.mouse.set_visible(1)
 pygame.display.set_caption("Darwin Duck!") # Set title
-window = pygame.display.set_mode([500,600]) # Set display size
-screen = pygame.display.get_surface()
+screen_width, screen_height = 500, 600
+screen=pygame.display.set_mode((screen_width, screen_height)) 
+# window = pygame.display.set_mode([500,600]) # Set display size
+# window = pygame.display.set_mode([500,600]) # Set display size
+# screen = pygame.display.get_surface()
 
 clock = pygame.time.Clock() # Initialize pygame clock
 
-player1 = duck(white,200,600)
+# Create platforms
+def create_level1(block_list,all_sprites_list):
+ 
+    for x in range(-600, 1500, 200):
+        block = Platform(white, 100, 20)
+        # Set x and y 
+        block.rect.x = x
+        block.rect.y = -100
+ 
+        block_list.add(block)
+        # all_sprites_list.add(block)
+ 
+    for x in range(-600, 1500, 200):
+        block = Platform(white, 100, 20)
+        # Set x and y 
+        block.rect.x = x
+        block.rect.y = 420
+ 
+        block_list.add(block)
+        # all_sprites_list.add(block)
 
+    for x in range(-500, 1500, 300):
+        block = Platform(blue, 100, 20)
+        # Set x and y 
+        block.rect.x = x
+        block.rect.y = 340
+ 
+        block_list.add(block)
+        allsprites.add(block_list)
+ 
+ 
+# Main program, create the blocks 
+block_list = pygame.sprite.Group()
+ 
 allsprites = pygame.sprite.OrderedUpdates() # Create group for all sprites
+ 
+create_level1(block_list,allsprites)
+
+
+player1 = duck(white,200,500)
+
 allsprites.add(player1)
 
 run = True
@@ -46,13 +100,44 @@ while run == True:
 				player1.right()
 				
 
+
+
 	# Draw graphics
 	screen.fill(bluewater) # Draw background
 	player1.update()
+	block_list.update()
+
+        if player1.rect.y <= 50:
+ 	   diff = 50 - player1.rect.y
+ 	   player1.rect.y = 50
+ 	   # player2.rect.y += diff
+ 	   for block in block_list:
+ 	       block.rect.y += diff
+     
+	if player1.rect.y >= screen_height:
+	   # player1.kill()
+ 	   # diff = player1.rect.y - 500
+ 	   player1.rect.y = screen_height-player1.rect[3]
+ 	   # player2.rect.y -= diff
+ 	   # for block in block_list:
+ 	   #     block.rect.y -= diff
+
+	if player1.rect.x <= 0:
+ 	   # diff = player1.rect.y - 500
+ 	   player1.rect.x = 0
+ 	   player1.xpos = 0
+
+	if player1.rect.x >= screen_width - player1.rect[2]:
+ 	   # diff = player1.rect.y - 500
+ 	   player1.rect.x = screen_width - player1.rect[2]
+ 	   player1.xpos = screen_width - player1.rect[2]
+
+
 	allsprites.draw(screen)
+	block_list.update()
 	
 	pygame.display.flip() # Redraw all graphics
-	clock.tick(50) # Run game at 50 fps
+	clock.tick(30) # Run game at 50 fps
 # - - - END MAIN LOOP - - -
 pygame.quit()
 sys.exit
