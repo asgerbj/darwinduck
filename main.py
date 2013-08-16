b7ab210b12df74e187d0ed3fde655f99d3f11f57
asgerbj <asgerbj@gmail.com>
2013-08-16 13:50:22 +0200

    The essentials added

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-hej med dmig
+# Import modules
+import os, sys
+import pygame
+from pygame.locals import * 
+
+if not pygame.font: print 'Warning, fonts disabled'
+if not pygame.mixer: print 'Warning, sound disabled'
+
+
+
