100bf1ff1b4ae2c8db51c3fde938132e54169754
jagob <jacobvdueholm@hotmail.com>
2013-08-16 14:04:37 +0200

    kan du merge det her

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ from pygame.locals import *
 if not pygame.font: print 'Warning, fonts disabled'
 if not pygame.mixer: print 'Warning, sound disabled'
 
-
-
+klsadkflsdkjf
+slakfjdsladkfj
+awklefjawlekæfj
