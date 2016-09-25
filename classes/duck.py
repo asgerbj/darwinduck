65297707802c7676eeccc0cb12081d799efd423e
jagob <jacobvdueholm@gmail.com>
2016-09-25 22:21:11 +0200

    less retarded movement, still retarded though

--- classes/duck.py
+++ classes/duck.py
@@ -3,203 +3,192 @@ import math
 import os
 
 class duck(pygame.sprite.Sprite):
-	def __init__(self, name, volume,x,y):
-		pygame.sprite.Sprite.__init__(self) 
-		self.name = name
-		self.image = pygame.image.load(os.path.join("images/%s/"%self.name, "N.png"))
-		self.rect = self.image.get_rect()
-		self.rect = self.rect.inflate(-10,-10)
-		self.rect.x = x
-		self.rect.y = y
-		self.xpos = float(x)
-		self.ypos = float(y)
-		self.movementdir = 0
-		self.movementspeed = 5
-		# Load sounds
-		self.sound_stone_hit = pygame.mixer.Sound(os.path.join('sound', 'stone_hit.wav'))
-		self.sound_stone_hit.set_volume(volume)
-		self.sound_duck1 = pygame.mixer.Sound(os.path.join('sound', 'duck_sound1.wav'))
-		self.sound_duck1.set_volume(volume)
-		self.sound_duck2 = pygame.mixer.Sound(os.path.join('sound', 'duck_sound2.wav'))
-		self.sound_duck2.set_volume(volume)
-		
+    def __init__(self, name, volume,x,y):
+        pygame.sprite.Sprite.__init__(self) 
+        self.name = name
+        self.image = pygame.image.load(os.path.join("images/%s/"%self.name, "N.png"))
+        self.rect = self.image.get_rect()
+        self.rect = self.rect.inflate(-10,-10)
+        self.rect.x = x
+        self.rect.y = y
+        self.xpos = float(x)
+        self.ypos = float(y)
+        self.old_x = 0
+        self.old_y = 0
 
-	def right(self):
-		self.movementspeed += 1
-                self.movementdir -= 30
-                if self.movementdir < 0:
-                   self.movementdir = 360 + self.movementdir
+        self.lookdir = 0
+        self.movementdir = 0
+        self.movementspeed = 5
+        self.maxspeed = 5
 
-	def left(self):
-		self.movementspeed += 1
-                self.movementdir += 30
-                if self.movementdir > 360:
-                   self.movementdir = self.movementdir - 360
+        # Load sounds
+        self.sound_stone_hit = pygame.mixer.Sound(os.path.join('sound', 'stone_hit.wav'))
+        self.sound_stone_hit.set_volume(volume)
+        self.sound_duck1 = pygame.mixer.Sound(os.path.join('sound', 'duck_sound1.wav'))
+        self.sound_duck1.set_volume(volume)
+        self.sound_duck2 = pygame.mixer.Sound(os.path.join('sound', 'duck_sound2.wav'))
+        self.sound_duck2.set_volume(volume)
 
-	def detectsprint(self, player_list, block_list, slow_list):
-		if self.rect.y <= 50:
-			pygame.sprite.Sprite.remove(self, player_list)
-			diff = 50 - self.rect.y
-			self.rect.y = 50
-			self.ypos = 50
-        # player2.rect.y += diff
-			for block in block_list:
-				block.rect.y += 3	
-			for block in slow_list:
-				block.rect.y += 3
-			for player in player_list:
-				player.rect.y += 4
-				player.ypos = player.rect.y
-			
-			player_list.add(self)
-	def playercollision(self, player_list):
-		pygame.sprite.Sprite.remove(self, player_list)
-		
-		# player_collide = pygame.sprite.spritecollide(self, player_list, False)
-		# if player_collide:
-		#     # We collided, go back to the old pre-collision location
-		#     self.rect.x = old_x
-		#     self.rect.y = old_y
-		#     self.xpos = old_x
-		#     self.ypos = old_y
-		
-		player_collision = pygame.sprite.spritecollide(self, player_list, False)
-		# speedvar = self.movementspeed
-		# dirvar = self.movementdir
-		collision_true = False
+    def right(self):
+        self.movementspeed += 1
+        if self.movementspeed > self.maxspeed:
+            self.movementspeed = self.maxspeed
+        elif self.movementspeed < -self.maxspeed:
+            self.movementspeed = -self.maxspeed
 
-		for player_collide in player_collision:
-			collision_true = True
+        self.lookdir -= 30
+        # self.movementdir -= 30
+        self.movementdir = self.lookdir
 
+        if self.lookdir < 0:
+            self.lookdir = 360 + self.lookdir
+        if self.movementdir < 0:
+            self.movementdir = 360 + self.movementdir
 
-			xtra = self.rect.x - player_collide.rect.x
-			ytra = self.rect.y - player_collide.rect.y
-			if ytra > 22*2/4 and xtra < ytra:
-				print "top   " + self.name
-				self.movementspeed -= player_collide.movementspeed * 0.5 + 1 
-				 
-			elif xtra > 30*2/4  and ytra < xtra:
-				print "left  " + self.name # hit on left
-				self.movementdir = 360 - self.movementdir
-				self.movementspeed += player_collide.movementspeed * 0.5 + 1
-				 
-			elif xtra < -30*2/4 and ytra > xtra:
-				print "right " + self.name
-				self.movementdir = (360 - self.movementdir) % 360
-				self.movementspeed += player_collide.movementspeed * 0.5 + 1
-				 
-			elif ytra < -22*2/4 :
-				print "bot   " + self.name
-				self.movementdir = player_collide.movementdir
-				self.movementspeed += player_collide.movementspeed * 0.5 + 1
-			else:
-				print "den burde ikke gaa herind "
-				print "xtra: " + str(xtra)
-				print "ytra: " + str(ytra)
+    def left(self):
+        self.movementspeed += 1
+        if self.movementspeed > self.maxspeed:
+            self.movementspeed = self.maxspeed
+        elif self.movementspeed < -self.maxspeed:
+            self.movementspeed = -self.maxspeed
 
-			# ride ride ranke
-			# if self.rect.x < player_collide.rect.x or player_collide.rect.x < self.rect.x:
-			# 	speedvar = self.movementspeed
-			# 	duckspeed = 0.5	
-			# 	if self.movementspeed > player_collide.movementspeed:
-			# 		player_collide.movementdir = self.movementdir
-			# 		self.movementspeed -= player_collide.movementspeed * 3
-			# 		player_collide.movementspeed = speedvar * duckspeed
-			# 	else: 
-			# 		self.movementdir = player_collide.movementdir 
-			# 		self.movementspeed = player_collide.movementspeed * duckspeed
-			# 		player_collide.movementspeed -= speedvar * 3
+        self.lookdir += 30
+        # self.movementdir += 30
+        self.movementdir = self.lookdir
 
-# VIRKER I Y
-			# if player_collide.rect.y > self.rect.y:
-			# 	self.movementdir = player_collide.movementdir
-			# 	self.movementspeed += player_collide.movementspeed * 0.5
-			# 	player_collide.movementspeed *= -0.3
+        if self.lookdir > 360:
+            self.lookdir = self.lookdir - 360
+        if self.movementdir > 360:
+            self.movementdir = self.movementdir - 360
 
-			# if player_collide.rect.y < self.rect.y:
-			# 	player_collide.movementdir =  self.movementdir 
-			# 	player_collide.movementspeed += self.movementspeed * 0.5
-			# 	self.movementspeed *= -0.3
+    def playercollision(self, player_list):
+        pygame.sprite.Sprite.remove(self, player_list)
 
-		player_list.add(self)
-		return collision_true 
+        player_collision = pygame.sprite.spritecollide(self, player_list, False)
+        collision_true = False
 
-	def update(self, player_list, block_list, slow_list):
-		old_x = self.rect.x
-		old_y = self.rect.y
+        multiplier = 1
+        for player_collide in player_collision:
+            collision_true = True
+            self.rect.x = self.old_x
+            self.rect.y = self.old_y
+            self.xpos   = self.old_x
+            self.ypos   = self.old_y
+            player_collide.rect.x = player_collide.old_x
+            player_collide.rect.y = player_collide.old_y
+            player_collide.xpos   = player_collide.old_x
+            player_collide.ypos   = player_collide.old_y
 
-		self.ypos -= self.movementspeed * math.cos(self.movementdir * math.pi / 180)
-		self.rect.y = int(self.ypos)
-		self.xpos -= self.movementspeed * math.sin(self.movementdir * math.pi / 180) 
-		self.rect.x = int(self.xpos)
+            if self.movementspeed > player_collide.movementspeed:
+                # constant to multiple the affect to push away ducks, avoid getting stuck in eachother
+                self.movementspeed -= (self.movementspeed - player_collide.movementspeed) * multiplier
+                # player_collide.movementdir   += self.movementdir
+                # tmp1 = abs(abs(player_collide.movementspeed) - abs(self.movementspeed))/max(abs(player_collide.movementspeed), abs(self.movementspeed))
+                player_collide.movementdir = self.movementdir
+                player_collide.movementspeed += (player_collide.movementspeed - self.movementspeed) * 1.5
+            else:
+                # Switch self and player
+                player_collide.movementspeed -= (player_collide.movementspeed - self.movementspeed) * 1.5
+                self.movementdir = player_collide.movementdir
+                self.movementspeed += (self.movementspeed - player_collide.movementspeed) * multiplier
 
-		slowed = pygame.sprite.spritecollide (self, slow_list, False)
-		for i in slowed:
-			self.movementspeed = self.movementspeed * 0.9 # Controls the slow effect
+            # speed cap
+            if self.movementspeed > self.maxspeed:
+                self.movementspeed = self.maxspeed
+            elif self.movementspeed < -self.maxspeed:
+                self.movementspeed = -self.maxspeed
+            if player_collide.movementspeed > self.maxspeed:
+                player_collide.movementspeed = self.maxspeed
+            elif player_collide.movementspeed < -player_collide.maxspeed:
+                player_collide.movementspeed = -player_collide.maxspeed
+        player_list.add(self)
+        return collision_true 
 
-		# if self.movementspeed > 0:
-		self.movementspeed =  self.movementspeed * 0.95
-		angle = 360 / 8 / 2
+    def update(self, player_list, block_list, slow_list):
+        old_x = self.rect.x
+        old_y = self.rect.y
+        self.old_x = old_x
+        self.old_y = old_y
 
-		if self.movementdir >= 360-angle or self.movementdir <= angle :
-			self.image = pygame.image.load(os.path.join("images/%s/"%self.name, "N.png"))
-						
-		elif self.movementdir >= 360 - 3 * angle :
-			self.image = pygame.image.load(os.path.join("images/%s/"%self.name, "NE.png"))
+        self.ypos -= self.movementspeed * math.cos(self.movementdir * math.pi / 180)
+        self.rect.y = int(self.ypos)
+        self.xpos -= self.movementspeed * math.sin(self.movementdir * math.pi / 180) 
+        self.rect.x = int(self.xpos)
 
-		elif self.movementdir >= 360 - 5 * angle:
-			self.image = pygame.image.load(os.path.join("images/%s/"%self.name, "E.png"))
+        slowed = pygame.sprite.spritecollide (self, slow_list, False)
+        for i in slowed:
+            self.movementspeed = self.movementspeed * 0.9 # Controls slow effect
 
-		elif self.movementdir >= 360 - 7 * angle:
-			self.image = pygame.image.load(os.path.join("images/%s/"%self.name, "SE.png"))
+        self.movementspeed =  self.movementspeed * 0.95
+        angle = 360 / 8 / 2
 
-		elif self.movementdir >= 360 - 9 * angle:
-			self.image = pygame.image.load(os.path.join("images/%s/"%self.name, "S.png"))
+        if self.lookdir >= 360-angle or self.lookdir <= angle :
+            self.image = pygame.image.load(os.path.join("images/%s/"%self.name, "N.png"))
+        elif self.lookdir >= 360 - 3 * angle :
+            self.image = pygame.image.load(os.path.join("images/%s/"%self.name, "NE.png"))
+        elif self.lookdir >= 360 - 5 * angle:
+            self.image = pygame.image.load(os.path.join("images/%s/"%self.name, "E.png"))
+        elif self.lookdir >= 360 - 7 * angle:
+            self.image = pygame.image.load(os.path.join("images/%s/"%self.name, "SE.png"))
+        elif self.lookdir >= 360 - 9 * angle:
+            self.image = pygame.image.load(os.path.join("images/%s/"%self.name, "S.png"))
+        elif self.lookdir >= 360 - 11 * angle:
+            self.image = pygame.image.load(os.path.join("images/%s/"%self.name, "SW.png"))
+        elif self.lookdir >= 360 - 13 * angle:
+            self.image = pygame.image.load(os.path.join("images/%s/"%self.name, "W.png"))
+        elif self.lookdir >= 360 - 15 * angle:
+            self.image = pygame.image.load(os.path.join("images/%s/"%self.name, "NW.png"))
 
-		elif self.movementdir >= 360 - 11 * angle:
-			self.image = pygame.image.load(os.path.join("images/%s/"%self.name, "SW.png"))
+        collide = pygame.sprite.spritecollide (self, block_list, False)
+        if collide:
+            # We collided, go back to the old pre-collision location
+            self.rect.x = old_x
+            self.rect.y = old_y
+            self.xpos = old_x
+            self.ypos = old_y
+            self.movementspeed = 0
+            if self.sound_last_collide == False:
+                self.sound_stone_hit.play()
+                self.sound_last_collide = True
+        else:
+            self.sound_last_collide = False
 
-		elif self.movementdir >= 360 - 13 * angle:
-			self.image = pygame.image.load(os.path.join("images/%s/"%self.name, "W.png"))
+        if self.rect.y >= 600 : # screen height
+            # diff = player1.rect.y - 500
+            self.rect.y = 600 - self.rect[3]
+            # player2.rect.y -= diff
+            # for block in block_list:
+            #     block.rect.y -= diff
 
-		elif self.movementdir >= 360 - 15 * angle:
-			self.image = pygame.image.load(os.path.join("images/%s/"%self.name, "NW.png"))
+        if self.rect.x <= 0:
+           # diff = player1.rect.y - 500
+           self.rect.x = 0
+           self.xpos = 0
 
-				# self.hitbox = self.rect.inflate(-5,-5)
-                collide = pygame.sprite.spritecollide (self, block_list, False)
-                if collide:
-                    # We collided, go back to the old pre-collision location
-                    self.rect.x = old_x
-                    self.rect.y = old_y
-                    self.xpos = old_x
-                    self.ypos = old_y
-                    self.movementspeed = 0
-                    if self.sound_last_collide == False:
-                       self.sound_stone_hit.play()
-                       self.sound_last_collide = True
-                else:
-                    self.sound_last_collide = False
-                
-                if self.rect.y >= 600 : # screen height
-                   # diff = player1.rect.y - 500
-                   self.rect.y = 600 - self.rect[3]
-                   # player2.rect.y -= diff
-                   # for block in block_list:
-                   #     block.rect.y -= diff
+        if self.rect.x >= 500 - self.rect[2]:
+           # diff = player1.rect.y - 500
+           self.rect.x = 500 - self.rect[2]
+           self.xpos = 500  - self.rect[2]
 
-                if self.rect.x <= 0:
-                   # diff = player1.rect.y - 500
-                   self.rect.x = 0
-                   self.xpos = 0
+        self.rect.y += 1
+        self.ypos += 1
 
-                if self.rect.x >= 500 - self.rect[2]:
-                   # diff = player1.rect.y - 500
-                   self.rect.x = 500 - self.rect[2]
-                   self.xpos = 500  - self.rect[2]
+        if self.rect.y > 600:
+            self.kill()
 
-                self.rect.y += 1
-                self.ypos += 1
+    def detectsprint(self, player_list, block_list, slow_list):
+        if self.rect.y <= 50:
+            pygame.sprite.Sprite.remove(self, player_list)
+            diff = 50 - self.rect.y
+            self.rect.y = 50
+            self.ypos = 50
 
-                if self.rect.y > 600:
-                   self.kill()
-                   
+            for block in block_list:
+                block.rect.y += 3	
+            for block in slow_list:
+                block.rect.y += 3
+            for player in player_list:
+                player.rect.y += 4
+                player.ypos = player.rect.y
+            player_list.add(self)
+
--- main.py
+++ main.py
@@ -30,7 +30,7 @@ screen=pygame.display.set_mode((screen_width, screen_height))
 pygame.mixer.init()
 clock = pygame.time.Clock() # Initialize pygame clock
 font = pygame.font.Font(None, 16) # Set font
-  
+
 # Main program, create the blocks 
 player_list = pygame.sprite.Group()
 block_list = pygame.sprite.Group()
@@ -39,11 +39,12 @@ allsprites = pygame.sprite.OrderedUpdates() # Create group for all sprites
 
 # Create platforms
 sys.path.append("levels")
+# from level1 import createlevel
 from level2 import createlevel
 createlevel(block_list, slow_list, allsprites, screen_width)
 
-duck1 = duck("rubberduck", volume,200,500)
-duck2 = duck("redhead", volume,300,500)
+duck1 = duck("redhead"   , volume, 200, 500)
+duck2 = duck("rubberduck", volume, 300, 500)
 
 player_list.add(duck1)
 player_list.add(duck2)
@@ -54,55 +55,59 @@ collision_true = False
 collision_true_counter = 0
 # - - - BEGIN MAIN LOOP - - -
 while run == True:
-	# Events
-	for event in pygame.event.get(): # Check for events
-		if event.type == pygame.QUIT: # If close-button is hit
-			run = False			
-		if event.type == pygame.KEYDOWN: # Keypresses
-			if event.key == pygame.K_ESCAPE:
-				run = False
-			
-			if event.key == pygame.K_RIGHT:
-				duck1.right()
-			if event.key == pygame.K_LEFT:
-				duck1.left()
-				
-			if event.key == pygame.K_d:
-				duck2.right()
-			if event.key == pygame.K_a:
-				duck2.left()
+    # Events
+    for event in pygame.event.get(): # Check for events
+        if event.type == pygame.QUIT: # If close-button is hit
+            run = False			
+        if event.type == pygame.KEYDOWN: # Keypresses
+            if event.key == pygame.K_ESCAPE:
+                run = False
+
+            if event.key == pygame.K_d:
+                duck1.right()
+            if event.key == pygame.K_a:
+                duck1.left()
+
+            if event.key == pygame.K_RIGHT:
+                duck2.right()
+            if event.key == pygame.K_LEFT:
+                duck2.left()
+
+    # Draw graphics
+    screen.fill(bluewater) # Draw background
+
+    block_list.update()
+    slow_list.update()
+    player_list.update(player_list, block_list, slow_list)
+
+    collision_true = duck1.playercollision(player_list)
+    # collision_true = duck2.playercollision(player_list)
+
+    # max_collisions = 2
+    # if collision_true_counter < 1 :
+    # 	collision_true = duck1.playercollision(player_list)
+    # 	# collision_true = duck2.playercollision(player_list)
+    # if collision_true:
+    # 	collision_true_counter += 1
+    # 	if collision_true_counter == 2:
+    # 	   collision_true_counter = 0
+    # 	   collision_true = False
 
 
 
-	# Draw graphics
-	screen.fill(bluewater) # Draw background
+    # duck1.detectsprint(player_list, block_list, slow_list)
+    # duck2.detectsprint(player_list, block_list, slow_list)
 
-	block_list.update()
-	slow_list.update()
-	player_list.update(player_list, block_list, slow_list)
 
-	if collision_true_counter < 1 :
-		collision_true = duck1.playercollision(player_list)
-		collision_true = duck2.playercollision(player_list)
-	if collision_true:
-		collision_true_counter += 1
-		if collision_true_counter == 20:
-		   collision_true_counter = 0
-		   collision_true = False
+    allsprites.draw(screen)
+    # block_list.update()
 
-	duck1.detectsprint(player_list, block_list, slow_list)
-	duck2.detectsprint(player_list, block_list, slow_list)
+    currentfps = int(clock.get_fps())
+    text_fps=font.render("fps: "+str(currentfps), True, white)
+    screen.blit(text_fps, (screen_width-50, 2))
 
-     
-	allsprites.draw(screen)
-	# block_list.update()
-	
-	currentfps = int(clock.get_fps())
-	text_fps=font.render("fps: "+str(currentfps), True, white)
-	screen.blit(text_fps, (screen_width-50, 2))
-	
-	pygame.display.flip() # Redraw all graphics
-	clock.tick(30) # Run game at 30 fps
+    pygame.display.flip() # Redraw all graphics
+    clock.tick(30) # Run game at 30 fps
 # - - - END MAIN LOOP - - -
 pygame.quit()
-sys.exit
+# sys.exit
